f06a39c5a428964fac44b218d1b0de74ccb47aa9
romode <friends.tv@web.de>
2022-03-13 14:47:14 +0100

    Added: login site style function
    Two functions were added to redisgn the admin login page with a external css file "login.css"

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -8,3 +8,19 @@ Author: DevStyles
 Version: 1.0
 Author URI: https://apptm.de
 */
+
+function modern_admin_dashboard_style() {
+    wp_enqueue_style('modern_admin_dashboard', plugins_url('wp-admin.css', __FILE__));
+}
+add_action('admin_enqueue_scripts', 'modern_admin_dashboard_style');
+add_action('login_enqueue_scripts', 'modern_admin_dashboard_style');
+
+
+
+function my_login_css() {
+    echo '<link rel="stylesheet" type="text/css" href="' .plugins_url('login.css  ', __FILE__). '">';
+  }
+  
+  add_action('login_head', 'my_login_css');
+
+?>
