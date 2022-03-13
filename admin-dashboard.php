a49119b573461fd96c6b64f7f1069b5470c1f632
romode <friends.tv@web.de>
2022-03-13 14:40:40 +0100

    Added Plugin information
    Plugin information was added to the main Plugin file
    -Plugin Name
    -Plugin
    -Description
    -Author
    -Version
    -Author

---
+++ admin-dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: Modern Admin Dashboard
+Plugin URI: https://apptm.de/modern-admin-dashboard
+Description: WordPress Admin Dashboard by DevStyles
+Author: DevStyles
+Version: 1.0
+Author URI: https://apptm.de
+*/
